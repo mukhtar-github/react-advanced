d27f1214a63e26c1f4b822b25477dabea2243e12
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-02 20:37:46 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/5-useState-counter.js';
+import Final from './tutorial/1-useState/final/3-useState-array.js';
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
+      <Final />
     </div>
   );
 }
