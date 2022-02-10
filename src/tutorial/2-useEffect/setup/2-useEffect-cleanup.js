09945eb3a9b1cd57a9afae8dd08f7853c0528dd3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-10 10:53:48 +0100

    Update file

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
   return <h2>useEffect cleanup</h2>;
 };
 
