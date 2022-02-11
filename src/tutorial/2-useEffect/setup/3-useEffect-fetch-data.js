5e1059c64c2f30fca6e887223f990d62144a59ae
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-11 17:25:16 +0100

    Update file

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -18,9 +18,17 @@ const UseEffectFetchData = () => {
   return (
     <>
      <h3>Github Users</h3>
-     {users.map(() => {
-       
-     })}
+     <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url} = user;
+          return <li key={id}>
+            <img src={avatar_url} alt={login}/>
+            <div>
+              <h4>{login}</h4>
+            </div>
+          </li>
+        })}
+     </ul>
     </>
   );
 };
