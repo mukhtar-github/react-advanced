64edad1a493cca6fc9f2577d217d82e30a127905
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-11 10:26:09 +0100

    Update file

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,12 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  return (
+    <>
+     <h3>Github Users</h3>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
