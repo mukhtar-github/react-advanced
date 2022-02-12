a6a77fe11577efccddeb0c6e227aec972dce4cf6
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-12 14:14:54 +0100

    Update file

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
   return <h2>multiple returns</h2>;
 };
