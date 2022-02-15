9da40ca592948ca3abd65098fbcd577724513933
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-15 08:25:39 +0100

    Update file

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -13,10 +13,20 @@ const ShowHide = () => {
 };
 
 const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+  }, []);
+
   return (
     <div style={{ marginTop: '2rem'}}>
       <h1>window</h1>
-      <h2>size : </h2>
+      <h2>size : {size} PX</h2>
     </div>
   )
 }
