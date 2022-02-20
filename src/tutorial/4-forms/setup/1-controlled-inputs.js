11d09fe6ee65a43e284639df5372b54f7e2dacf3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-20 08:53:57 +0100

    Update file

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,12 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  return (
+  <article>
+    <form className='form'>
+      <div className="form-control"></div>
+    </form>
+  </article>);
 };
 
 export default ControlledInputs;
