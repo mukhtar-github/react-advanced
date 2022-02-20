4560ff7e413b466dd09d802211e3081bdea641a0
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-20 09:29:03 +0100

    Update file

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -17,6 +17,7 @@ const ControlledInputs = () => {
         <label htmlFor='email'>Email : </label>
         <input type='text' id='email' name='email' />
       </div>
+      <button type='submit'>add person</button>
     </form>
   </article>);
 };
