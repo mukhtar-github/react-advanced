8520eb6d480f502a1057c11c7c4d7ae9adac9d8d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-22 04:34:50 +0100

    Update file

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -51,10 +51,10 @@ const ControlledInputs = () => {
       <button type='submit'>add person</button>
     </form>
     {
-      people.map((person, index) => {
+      people.map((person) => {
         const { id, firstName, email } = person;
         return (
-          <div className='item'>
+          <div className='item' key={id}>
             <h4>{firstName}</h4>
             <p>{email}</p>
           </div>
