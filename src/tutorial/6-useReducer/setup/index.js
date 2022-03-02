c9e1a2c250331f62852f5e8da7237a8dfa007ba8
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-02 04:26:19 +0100

    Update file

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -5,7 +5,8 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-const [people, setPeople] = useState();
+const [people, setPeople] = useState(data);
+const [showModal, setShowModal] = useState(false);
 
   return <h2>useReducer</h2>;
 };
