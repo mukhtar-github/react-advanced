82a0bff4eb2b90584c9dfcdd70880ab731f0e645
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-02 04:37:53 +0100

    Update file

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -5,10 +5,22 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-const [people, setPeople] = useState(data);
-const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
 
-  return <h2>useReducer</h2>;
+  return (
+    <>
+      {showModal && <Modal/>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text' 
+          value={name} 
+          onChange={() => setName(e.target.value)}
+        />
+      </form>
+    </>
+  );
 };
 
 export default Index;
