e4694a49d9525c220510cbc0f1f0fa4bdfd923bb
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-09 05:20:54 +0100

    Update files

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,36 +1,7 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-
-// reducer function
-const reducer = (state, action) => {
-  if (action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state, 
-      people: newPeople, 
-      isModalOpen: true, 
-      modalContent: 'Item added'
-    };
-  }
-  if (action.type === 'NO_VALUE') {
-    return { 
-      ...state,
-      isModalOpen: true,
-      modalContent: 'please enter value'
-    };
-  }
-  if (action.type === 'CLOSE_MODAL') {
-    return { ...state, isModalOpen: false };
-  }
-  if (action.type === 'REMOVE_ITEM') {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return { ...state, people: newPeople };
-  }
-  throw new Error('no matching action type');
-};
+import { reducer } from './reducer';
 
 const defaultState = {
   people: [],
