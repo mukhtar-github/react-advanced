406930b82dea59e2c0e9fda2ba15420d1302fdb7
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-09 05:03:45 +0100

    Update file

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -21,7 +21,10 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === 'CLOSE_MODAL') {
-    return { ...state, isModalOpen: false }
+    return { ...state, isModalOpen: false };
+  }
+  if (action.type === 'REMOVE_ITEM') {
+    const newPeople = state.people.filter((person) => person.id !== action.payload);
   }
   throw new Error('no matching action type');
 };
