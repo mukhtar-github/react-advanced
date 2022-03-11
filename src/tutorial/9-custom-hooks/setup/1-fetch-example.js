394bfae592916ac3bc2d8486a9845553554395e5
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-11 17:21:22 +0100

    Update file

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './2-useFetch'
+import React, { useState, useEffect } from 'react';
+import { useFetch } from './2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    getProducts()
-  }, [url])
-  console.log(products)
+    getProducts();
+  }, [url]);
+  console.log(products);
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
@@ -27,4 +27,4 @@ const Example = () => {
   )
 }
 
-export default Example
+export default Example;
