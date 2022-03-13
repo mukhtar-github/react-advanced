a8bd7305d47a1d6de1bc9fb0cf2b4c6ee0e2e054
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-13 14:31:02 +0100

    Update file

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Product = ({ image, name, price }) => {
   return (
     <article className='product'>
+      <img src={image.url} alt={name} />
       <h4>{name}</h4>
+      <p>${price}</p>
     </article>
   );
 };
