902d198634735cc07811d3e4ecde6775610cd953
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-13 16:42:31 +0100

    Update file

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types';
 import defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
-  console.log({image, name, price});
+  console.log(image, name, price);
   return (
     <article className='product'>
-      <h4>single product</h4>
       <img src={image.url} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
