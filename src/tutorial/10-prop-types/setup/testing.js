84163fb7a3472a99f8130675b8c9b6a5e35579b6
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-14 06:44:28 +0100

    Update file

---
+++ src/tutorial/10-prop-types/setup/testing.js
@@ -0,0 +1,14 @@
+// ES7+React/Redux/React-Native snippets
+// rafcp - ReactArrowFunctionComponentwithPropTypes
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const testing = props => {
+  return (
+    <div>testing</div>
+  )
+}
+
+testing.propTypes = {}
+
+export default testing
