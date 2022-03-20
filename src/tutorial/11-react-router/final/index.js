67e3d1968607a4a9585bfa91afe9fe4d687a9ac8
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-20 09:30:13 +0100

    Update file

--- src/tutorial/11-react-router/final/index.js
+++ src/tutorial/11-react-router/final/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // react router
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // pages
 import Home from './Home';
 import About from './About';
@@ -9,6 +9,7 @@ import Error from './Error';
 import Person from './Person';
 // navbar
 import Navbar from './Navbar';
+
 const ReactRouterSetup = () => {
   return (
     <Router>
