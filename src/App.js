37c4f1c764c92b0efa9fe00d5b654b8201bf1144
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-20 11:18:20 +0100

    Update file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Final from './tutorial/11-react-router/final';
+import Setup from './tutorial/11-react-router/setup';
 
 function App() {
   return (
     <div className='container'>
-      <Final />
+      <Setup />
     </div>
   );
 }
