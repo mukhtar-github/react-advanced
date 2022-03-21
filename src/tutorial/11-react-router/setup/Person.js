95f60367b22f030d4206aa88deed65a3128fc93d
Mukhtar <mukhtartanimugarba@gmail.com>
2022-03-21 06:38:25 +0100

    Update file

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -5,10 +5,15 @@ import { Link, useParams } from 'react-router-dom';
 const Person = () => {
   const [name, setName] = useState('default name');
   const {id} = useParams();
+
+  useEffect(() => {
+    const newPerson = data.find((person) => person.id === parseInt(id));
+  }, []);
+
   return (
     <div>
       <h1>{name}</h1>
-      <Link to='/people'>
+      <Link to='/people' className='btn'>
         Back to People
       </Link>
     </div>
